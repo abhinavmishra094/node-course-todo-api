e5e14561397e8c70a159fc1e402bcc77bb29cc01
Sayeekrishnan <sayee@kalajitha.com>
2018-07-30 11:07:25 +0530

    /todod/id added

--- sever/server.js
+++ sever/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 var {mongoose } = require('./db/mongoose');
 var {Todo } = require('./models/todo');
@@ -16,7 +17,7 @@ app.post('/todos',(req,res)=>{
   },(e)=>{
     res.status(400).send(e);
   });
-})
+});
 
 app.get('/todos',(req,res)=>{
   Todo.find().then((todo)=>{
@@ -24,6 +25,19 @@ app.get('/todos',(req,res)=>{
   },(e)=>{
       res.status(400).send(e);
   });
+});
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  if(!ObjectId.isValid(id)){
+    res.sendStatus(404).send({});
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.sendStatus(400).send({});
+    }
+    res.send(todo);
+  }).catch((e)=> res.sendStatus(400).send({}));
+
 });
 app.listen(3000,()=>{
   console.log('started on posrt 3000');
