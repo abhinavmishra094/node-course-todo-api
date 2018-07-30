90342f9e83eefbc4734d0cda9b459a2a898ada96
Sayeekrishnan <sayee@kalajitha.com>
2018-07-30 09:45:15 +0530

    test for /todos post added

--- sever/tests/server.test.js
+++ sever/tests/server.test.js
@@ -7,26 +7,42 @@ const {Todo} = require('./../models/todo');
 beforeEach ((done)=>{
   Todo.remove({}).then(()=>done());
 });
-describe('Post /todo',()=>{
+describe('Post /todos',()=>{
   it('should create a new todo',(done)=>{
     var text = 'Test todo text';
 
     request(app)
-    .post('/todo')
+    .post('/todos')
     .send({text})
     .expect(200)
-    .exec((res)=>{
+    .expect((res)=>{
       expect(res.body.text).toBe(text);
     })
     .end((err,res)=>{
       if(err){
-        done(err);
+        return done(err);
       }
       Todo.find().then((todos)=>{
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
+        done();
       }).catch((e)=>done(e))
     });
+  });
 
+  it('should not  create todo with invalid data',(done)=>{
+    request(app)
+    .post('/todos')
+    .send({})
+    .expect(400)
+    .end((err,res)=>{
+      if(err){
+        return done(err);
+      }
+      Todo.find().then((todos)=>{
+        expect(todos.length).toBe(0);
+        done();
+      }).catch((e)=>done(e))
+    });
   });
 })
