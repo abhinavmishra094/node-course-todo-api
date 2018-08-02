c7f0dd469ceb0ed96b64f53e1e31353196e129c9
Sayeekrishnan <sayee@kalajitha.com>
2018-08-02 17:05:48 +0530

    password hashin added

--- playgorund/hashing.js
+++ playgorund/hashing.js
@@ -1,13 +1,26 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id:10
-}
-var token = jwt.sign(data,'123abc');
-console.log(token);
-var decoded = jwt.verify(token,'123abc');
-console.log(decoded);
+var password = 'abc123!';
+
+// bcrypt.genSalt(10,(err,salt)=>{
+//   bcrypt.hash(password,salt,(err,hash)=>{
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$8Xex8X7ImhZBKA/xj9PJheUQooaebW8Zwk.PdBkXcTvKOYBKYe3NK';
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+  console.log(res);
+});
+// var data = {
+//   id:10
+// }
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+// var decoded = jwt.verify(token,'123abc');
+// console.log(decoded);
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 //
