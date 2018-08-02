ac2959d3c012012fa0e9e8c631e7abb09d9959f9
Sayeekrishnan <sayee@kalajitha.com>
2018-08-02 16:11:12 +0530

    private route getUser added

--- sever/server.js
+++ sever/server.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser');
 const {ObjectId} = require('mongodb');
 
 var {mongoose } = require('./db/mongoose');
-var {Todo } = require('./models/todo');
-var {User } = require('./models/user');
-
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate')
 var app = express();
 const port = process.env.PORT ;
 
@@ -93,6 +93,10 @@ app.post('/users',(req,res)=>{
   }).catch((e)=>{
     res.sendStatus(400).send(e);
   });
+});
+
+app.get('/users/me',authenticate,(req,res)=>{
+  res.send(req.user);
 });
 app.listen(port,()=>{
   console.log(`started on port ${port}`);
